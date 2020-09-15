b132bf08c94de49a797b4ae7f7026e7f9f2cebb1
Shamil K <shajdaro@kpfu.ru>
2020-09-15 15:17:12 +0300

    add project

---
+++ src/main/java/ru/kpfu/classwork/lesson_01/Lesson_01.java
@@ -0,0 +1,12 @@
+package ru.kpfu.classwork.lesson_01;
+
+/**
+ * Lesson 1.
+ * @author Shamil Khaydarov
+ */
+
+public class Lesson_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
