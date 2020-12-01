44abe798c96034738ab933b65b3b41593dab8bf4
Shamil Khaydarov <hsh0@ya.ru>
2020-12-01 15:45:49 +0300

    add filesexamples

--- src/main/java/ru/kpfu/classwork/FileExamples/Test.java
+++ src/main/java/ru/kpfu/classwork/FileExamples/Test.java
@@ -55,12 +55,35 @@ public class Test {
 
     }
 
+
+    public static void copyMinLines(String filename) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(filename.replace(".txt", "_2.txt"))));
+        String line = null;
+
+        int minLength = Integer.MAX_VALUE;
+        StringBuilder sb = new StringBuilder();
+        while (br.ready() && (line = br.readLine()) != null) {
+            if (line.length() < minLength) {
+                minLength = line.length();
+//                sb.delete(0, sb.length());
+                pw.write("");
+            }
+            if (line.length() == minLength) {
+//                sb.append(line).append("\r\n");
+                pw.println(line);
+            }
+        }
+        pw.close();
+    }
+
     public static void main(String[] args) {
         String path = "src/main/java/ru/kpfu/classwork/FileExamples/";
         String filename = path + "ints.txt";
         try {
             copyIntReverse(filename);
             copyToSmallest(path + "1.jpg", path + "2.jpg", path + "3.jpg");
+            copyMinLines(path + "file64.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
