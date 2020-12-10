4418d60fd570ca0429732dc500bb711f11ff968e
Shamil Khaydarov <hsh0@ya.ru>
2020-12-10 11:45:15 +0300

    game 2

--- src/main/java/ru/kpfu/classwork/ChainsGame/Test.java
+++ src/main/java/ru/kpfu/classwork/ChainsGame/Test.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 public class Test {
 
     private static String[] dictionary;
+    private static String[] history;
+    private static int historyLength = 0;
 
     private static void readDictionary(String filename) throws IOException {
         File file = new File(filename);
@@ -15,6 +17,7 @@ public class Test {
         int counter = 0;
 
         dictionary = new String[(int) size];
+        history = new String[(int) size];
         int i = 0;
         while ((line = br.readLine()) != null) {
             dictionary[i++] = line;
@@ -22,15 +25,43 @@ public class Test {
         br.close();
     }
 
+    /*
+    * left < right => -1 or negative
+    * left == right => 0
+    * left > right => 1 or positive
+    *
+    * */
+
+    // клен клон
+
+    public static int compare(String left, String right) {
+        int minLength = Math.min(left.length(), right.length());
+        int c1, c2;
+        for (int i = 0; i < minLength; i++) {
+            c1 = left.charAt(i) * 2; // 184*2
+            c2 = right.charAt(i);
+
+            c2 = c2 == 'ё' ? 'е' * 2 + 1 : c2 * 2;
+            if (c1 == 'ё' * 2) c1 = 'е' * 2 + 1;
+
+//            if (c1 < c2) return -1;
+//            if (c1 > c2) return 1;
+
+            if (c1 != c2) {
+                return c1 - c2;
+            }
+        }
+        return left.length() - right.length();
+    }
 
     public static boolean binarySearch(String findMe, String[] sorted) {
         int left = 0;
         int right = sorted.length - 1;
         while (left <= right) {
             int middle = left + (right - left) / 2;
-            if (sorted[middle].compareTo(findMe) < 0) {
+            if (compare(sorted[middle], findMe) < 0) {
                 left = middle + 1;
-            } else if (sorted[middle].compareTo(findMe) > 0) {
+            } else if (compare(sorted[middle], findMe) > 0) {
                 right = middle - 1;
             } else {
                 return true;
@@ -50,32 +81,64 @@ public class Test {
                     char[] word = current.toCharArray();
                     word[i] = abc.charAt(j);
                     current = String.valueOf(word);
-                    if (!current.equals(begin) && binarySearch(current, dictionary)) {
+                    if (!current.equals(begin) && !inArray(current, history, historyLength)
+                            && binarySearch(current, dictionary)) {
                         sb.append(current).append(",");
+                        history[historyLength] = current;
+                        historyLength++;
                     }
                 }
             }
         }
+        if (sb.length() == 0) return new String[0];
         return sb.toString().split(",");
     }
-    public static void game(String begin, String end) {
+
+    private static boolean inArray(String findMe, String[] history, int size) {
+        for (int i = 0; i < size; i++) {
+            if (findMe.equals(history[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean found = false;
+
+    private static void game(String begin, String end, String chain) {
         if (begin.equals(end)) {
-            System.out.println("Found!");
             System.out.println(end);
+            System.out.println("Found!");
+            chain += end;
+            System.out.println("Цепочка найдена: " + chain);
+            found = true;
             return;
         }
-
+        history[historyLength++] = begin;
         System.out.print(begin + ": ");
         String[] words = getWords(begin, end);
+        if (words.length > 0) {
+            chain += begin + " - ";
+        }
         System.out.println(Arrays.toString(words));
 
         for (int i = 0; i < words.length; i++) {
-            game(words[i], end);
+            if (found) return;
+            game(words[i], end, chain);
         }
 
     }
 
+    public static void game(String begin, String end) {
+        game(begin, end, "");
+    }
+
     public static void main(String[] args) {
+//
+//        String[] test = new String[] {"ель", "ёжик", "ёлка", "клён", "клон", "стен"};
+//
+//        System.out.println("Search yozik: ");
+//        System.out.println(binarySearch("клён", test));
         // муха => слон
         // стук - сток - стон - слон
         String begin = "стук";
